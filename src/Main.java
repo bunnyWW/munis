a99bd6dce705bec862fddca6753fddf9fc9b6079
bunny <aabdujalil2009@gmail.com>
2024-08-05 11:47:13 +0500

    Salom

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("212");
+        int i =
     }
 }
